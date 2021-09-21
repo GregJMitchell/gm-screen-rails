2f741af2eae993c405f066ae2352543c04cdcd52
GregJMitchell <g2mitchell3003@gmail.com>
2021-09-20 22:27:29 -0600

    Create campaign table

---
+++ db/migrate/20210921042510_create_campaigns.rb
@@ -0,0 +1,11 @@
+class CreateCampaigns < ActiveRecord::Migration[6.1]
+  def change
+    create_table :campaigns do |t|
+      t.string :title
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/campaigns.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: campaigns
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_campaigns_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :campaign do
+    title { "MyString" }
+    description { "MyText" }
+    user { nil }
+  end
+end
