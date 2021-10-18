f6b5c640a702fd3faab79cc1205233f30aec5e8f
GregJMitchell <g2mitchell3003@gmail.com>
2021-10-17 22:43:48 -0600

    Add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :campaigns
+  has_many :campaigns, dependent: :destroy
 end
