16043ea504d3b2f56f34ffb11b30406959b16748
GregJMitchell <g2mitchell3003@gmail.com>
2021-10-24 11:55:00 -0600

    New models

---
+++ app/models/size_category.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: size_categories
+#
+#  id         :bigint           not null, primary key
+#  grid_area  :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class SizeCategory < ApplicationRecord
+  validates :name, presence: :true
+end
