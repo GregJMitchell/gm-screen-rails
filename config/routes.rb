d0eb2f4cbcd61a2d55fa13dd69120d82dcd55831
GregJMitchell <g2mitchell3003@gmail.com>
2021-10-24 11:53:59 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  resources :campaigns
+  resources :campaigns do
+    resources :player_characters, except: %i[index], controller: 'campaigns/player_characters'
+    resources :non_player_characters, except: %i[index], controller: 'campaigns/non_player_characters'
+  end
 end
