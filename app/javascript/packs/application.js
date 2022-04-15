1c4080fea6361a494207bfa791542796c890fd1c
Gregory Mitchell <g2mitchell3003@gmail.com>
2022-04-15 09:09:29 -0600

    Install turbo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@ window.jQuery = $;
 window.$ = $;
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
