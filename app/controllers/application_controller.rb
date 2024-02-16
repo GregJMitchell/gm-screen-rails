6342b473c118c1d879e11f9eef2a47fc447ff8ef
Greg Mitchell <g2mitchell3003@gmail.com>
2024-02-15 20:26:20 -0700

    Inlcude pagy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Breadcrumber
+  include Pagy::Backend
 
   def is_current_user
     redirect_to new_user_session_path unless current_user
